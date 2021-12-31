6f42d77fabbf4bfc700c23f552dabc830ff35f1d
smartcatboy <smartcatboy@tuta.io>
2021-12-31 15:26:40 +0800

    feat: prepare for auto update

--- upgrade.sh
+++ upgrade.sh
@@ -2,24 +2,31 @@
 
 # For test
 #BACKEND_VERSION="0.15.4"
-#AUTO_UPGRADE="yes"
+AUTO_UPGRADE="yes"
 
+LOCAL=`pwd`
 UPDATE_URL=https://raw.githubusercontent.com/smartcatboy/deploy/main/VERSION
 VERSION_INFO=`wget -qO - $UPDATE_URL | grep -v ^\# | grep -w ^$BACKEND_VERSION`
 LATEST_VERSION=`echo $VERSION_INFO | awk '{print $2}'`
-LATEST_ID=`echo $VERSION_INFO | awk '{print $3}'`
-FORCE=`echo $VERSION_INFO | awk '{print $4}'`
 if [[ -z $LATEST_VERSION ]]; then
     echo "No need to upgrade."
     exit 0
 fi
+LATEST_ID=`echo $VERSION_INFO | awk '{print $3}'`
+if [[ $LATEST_ID == "#" ]]; then
+    rm -rf /tmp/compose
+    LATEST_ID=`git clone -q https://github.com/smartcatboy/compose.git /tmp/compose && \
+    cd /tmp/compose && git checkout -q tags/$LATEST_VERSION && \
+    git ls-files -s backend | grep -w backend$ | awk '{print $2}'`
+fi
+FORCE=`echo $VERSION_INFO | awk '{print $4}'`
 if [[ $FORCE == "N" && $AUTO_UPGRADE != "yes" ]]; then
     echo "No need to upgrade."
     exit 0
 fi
 
 echo "Start upgrading to $LATEST_VERSION ($LATEST_ID) ..."
-git fetch && git reset --hard $LATEST_ID
+cd $LOCAL && git fetch && git reset --hard $LATEST_ID
 if [ $? -eq 0 ]; then
     echo "Upgrade successfully!"
     exit 0
@@ -27,4 +34,3 @@ else
     echo "Upgrade failed!"
     exit 1
 fi
-
