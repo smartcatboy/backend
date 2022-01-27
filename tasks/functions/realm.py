3cdde136cdb6a5f47e82bd1d5c0513ad6b9ffe89
smartcatboy <smartcatboy@tuta.io>
2022-01-27 16:46:07 +0800

    fix: realm udp

--- tasks/functions/realm.py
+++ tasks/functions/realm.py
@@ -31,7 +31,7 @@ class RealmConfig(AppConfig):
         return (
             f"/usr/local/bin/realm "
             f"-l 0.0.0.0:{port.num} "
-            f"-r {remote_address}:{remote_port}"
+            f"-uzr {remote_address}:{remote_port}"
         )
 
     @property
